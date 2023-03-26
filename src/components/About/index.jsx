c203cbd7ea3e2ea311ec57644d3d26e6b5724cac
superd <im_dd@qq.com>
2023-03-26 23:09:58 +0800

    React学习

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,14 +3,18 @@ import {Route,Switch,Redirect} from 'react-router-dom'
 import MyNavLink from '../MyNavLink'
 import Home from '../Home'
 export default class About extends Component {
+  replaceLink(){
+      this.props.history.replace("/about/xxx")
+  }
+   
   render() {
     return (
       <div>
         <div className="route">
-          <MyNavLink to={`/about/news/1/你好胖`}>News</MyNavLink>
+          <MyNavLink replace to={`/about/news/1/你好胖`}>News</MyNavLink>
           {/* 传递search参数，正常注册路由 */}
-          <MyNavLink to="/about/msg?id=1&info=yes">Messages</MyNavLink>
-          <MyNavLink to={
+          <MyNavLink replace to="/about/msg?id=1&info=yes">Messages</MyNavLink>
+          <MyNavLink replace to={
             {
               pathname:"/about/state",
               state:{
